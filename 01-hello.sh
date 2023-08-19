9ff3fde5589b9b7217cf8a2b01ea5d35ed03c33c
sridharkamilla <sridhar.kamilla@gmail.com>
2023-08-19 20:49:23 +0530

    abcd

--- 01-hello.sh
+++ 01-hello.sh
@@ -3,9 +3,11 @@
 echo "Hello World, I am learning DevOps with AWS"
 PERSON1=Sachin
 PERSON2=Rohit
+PERSON3=Sridhar
 
 echo "$PERSON1: Hi $PERSON2, Good Morning"
 echo "$PERSON2: Good Morning $PERSON1"
 echo "$PERSON1: How are you $PERSON2"
 echo "$PERSON2: i am good, how are you $PERSON1"
-echo " Now i am updating the code into git hub"
+echo " $PERSON3: is a new persom to join the group"
+echo "The End."
