478862ba91cb842bfa6b41dc6c695041ee4faef0
sridharkamilla <sridhar.kamilla@gmail.com>
2023-08-19 21:11:57 +0530

    three

--- 03-variables.sh
+++ 03-variables.sh
@@ -4,5 +4,7 @@ Number1=$1
 Number2=$2
 Number3=$3
 
-Sum1= $(Number1+Number2+Number3)
-Echo " : Sum of three number :: &sum1"
+SUM=$((Number1+Number2+Number3))
+#SUM=$((NUMBER1+NUMBER2))
+
+Echo " : Sum of three number :: &SUM"
