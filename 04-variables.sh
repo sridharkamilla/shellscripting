687f3ffb907fd036c027fe39e82865d2e37fe9e3
sridharkamilla <sridhar.kamilla@gmail.com>
2023-08-19 21:54:25 +0530

    four variables

---
+++ 04-variables.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo " enter username :"
+read -s USERNAME 
+
+Echo " User has entered : $USERNAME
+
+echo " Enter Password"
+
+read -s PASSWORD
+
+Echo " User has entered : $PASSWORD
